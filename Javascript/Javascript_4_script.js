0d18d46f9c98bdb3305b10cab32c60f37bac9dca
navanitnandakumar <navanitnandakumar@gmail.com>
2023-08-13 16:46:45 +0530

    Updated files

--- Javascript/Javascript_4_script.js
+++ Javascript/Javascript_4_script.js
@@ -1,6 +1,6 @@
 // 'While' loop :-
 
-// while (condition){
+// while (condition) {
 //     Code to be exectuted as long as condition holds
 // }
 
@@ -28,11 +28,34 @@ while (x <= 10) {
     if (x % 2 == 0) {
         console.log(x);
     }
-    x = x + 1;
+    x++;
 }
 
 x = 2;
 while (x <= 10) {
     console.log(x);
-    x = x + 2;
+    x += 2;
+}
+
+// 'For' loop :-
+
+// for (statement 1; statement 2; statement 3) {
+//     Code to be exectuted as long as condition holds
+// }
+
+// 'statement 1' is executed before the loop even begins (eg: var i = 0)
+// 'statement 2' is the condition for the loop (eg: i<5)
+// 'statement 3' is executed each time after the loop cycles through (eg: i++)
+
+for (var i = 0; i < 5; i++) {
+    console.log("Number is " + i);
+}
+
+var word = 'ABCDEFGHIJK'
+for (let i = 0; i < word.length; i++) {
+    console.log(word[i]);
+}
+
+for (let i = 0; i < word.length; i += 2) {
+    console.log(word[i]);
 }
